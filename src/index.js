9c7ad414069ada27d173a5883a32767e659eae88
Brett Fowler <b16fowler@gmail.com>
2025-07-15 13:27:48 -0400

    Renamed certain elements for clarity + new function to handle sub button actions

--- src/index.js
+++ src/index.js
@@ -1,35 +1,49 @@
 /**************************************************************************
  * index.js
- * 
+ *
  * To do:
  * Fix pathing? issue on embedded files
  * Hide toolbar button
  * Better CSS
- * 
+ *
  **************************************************************************/
 
 function handleTabClick(e, tab) {
   e.preventDefault();
 
   // Element of tab that was clicked
-  const tabLink = document.querySelector(`#${tab}-tab-link`);
+  const tabButton = document.querySelector(`#${tab}-tab-button`);
   // Tab-content of tab that was clicked
   const tabContent = document.querySelector(`#${tab}-content`);
 
   // Make all tabs inactive
-  const tabs = document.querySelectorAll(".tab-link");
-  tabs.forEach((tab) => {
+  const tabs = document.querySelectorAll(".tab-button");
+  tabs.forEach(tab => {
     tab.classList.remove("active");
   });
 
   // Hide all tab content
   const tabContents = document.querySelectorAll(".tab-content");
 
-  tabContents.forEach((tab) => {
+  tabContents.forEach(tab => {
     tab.style.display = "none";
   });
 
   // Add active to correct tab's class and unhide that tab's content
-  tabLink.classList.toggle("active");
+  tabButton.classList.toggle("active");
   tabContent.style.display = "block";
+
+  // If user clicked on 'cert' tab, trigger animation for both sub buttons
+  if (tab === "cert") {
+    const subButtons = document.querySelectorAll(".sub-button");
+    subButtons.forEach(button => {
+      button.classList.add("animate");
+    });
+  }
+}
+
+function handleSubClick(e, button) {
+  e.preventDefault();
+
+  console.log(button);
 }
